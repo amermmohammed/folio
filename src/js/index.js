fa85a910602165ac530dc4aab2e8deacfc6c102b
Amer Malik Mohammed <amermalikmohammed@gmail.com>
2021-07-08 22:36:29 +0200

    Add skiles section update cv

--- src/js/index.js
+++ src/js/index.js
@@ -100,14 +100,16 @@ $('.switch-mode').click(function() {
     $("html").toggleClass("light1");
     $("body").toggleClass("light1");
     $("#Info").toggleClass("light1");
+    $("#skile").toggleClass("light1");
     $("#Experience").toggleClass("light1");
     $("#Education").toggleClass("light1");
     $("#Portfolio").toggleClass("light1");
-    $("#Wall").toggleClass("light1");
+    $("#Achievements").toggleClass("light1");
     $("#Contact").toggleClass("light1");
     $("#Footer").toggleClass("light1");
     $(".custom-card").toggleClass("light2");
     $(".card").toggleClass("light2");
+    $(".skile-row").toggleClass("light3");
     $(".gallarycard").toggleClass("light2");
     $(".light-row").toggleClass("light3");
     $(".light-conatainer").toggleClass("light3");
@@ -116,22 +118,63 @@ $('.switch-mode').click(function() {
     $(this).toggleClass("active");
 });
 
+// typewriter
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-//copy-right
-
-var date = new Date();
-var year = date.getFullYear();
-document.getElementById("date").innerHTML = " All rights reserved " + year + "&copy;";
